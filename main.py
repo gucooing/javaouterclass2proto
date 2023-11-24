76c4d00eb7af5d4a133b9643ad2b4cab6150dfa6
ノエルの犬 <90658478+gucooing@users.noreply.github.com>
2023-11-24 12:36:29 +0800

    捕获错误

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 import os
+import logging
+
+# 设置日志记录格式
+logging.basicConfig(filename='error.log', level=logging.ERROR, format='%(asctime)s - %(message)s')
 
 root_dir = 'input'
 
@@ -26,32 +30,42 @@ for dirpath, dirnames, filenames in os.walk(root_dir):
             shit = file_to_save_to.replace('.proto', '').replace('output\\', '')
             o.write(f'syntax = "proto3";\n\noption java_package = "emu.grasscutter.net.proto";\n\n')
             def doimport():
-                for line in lines_set:
-                    if "<code>" in line:
-                        if not any(key in line for key in keys):
-                            someshit = (str(line)
-                                .replace('     * <code>', '')
-                                .replace('.', '')
-                                .replace('</code>', '')
-                                .replace('* <code>', '')
-                                .replace('&lt;', ' <')
-                                .replace('&gt;', '>')
-                                .replace(' = ', '')
-                                .replace(';', '')[:-1]
-                                .replace('0', '')
-                                .replace('1', '')
-                                .replace('2', '')
-                                .replace('3', '')
-                                .replace('4', '')
-                                .replace('5', '')
-                                .replace('6', '')
-                                .replace('7', '')
-                                .replace('8', '')
-                                .replace('9', '')
-                            )
-                            somewords = someshit.split()
-                            second_word_from_right = somewords[-2]
-                            o.write('import "'+second_word_from_right+'.proto";\n')
+                try:
+                    for line in lines_set:
+                        if "<code>" in line:
+                            if not any(key in line for key in keys):
+                                someshit = (str(line)
+                                    .replace('     * <code>', '')
+                                    .replace('.', '')
+                                    .replace('</code>', '')
+                                    .replace('* <code>', '')
+                                    .replace('&lt;', ' <')
+                                    .replace('&gt;', '>')
+                                    .replace(' = ', '')
+                                    .replace(';', '')[:-1]
+                                    .replace('0', '')
+                                    .replace('1', '')
+                                    .replace('2', '')
+                                    .replace('3', '')
+                                    .replace('4', '')
+                                    .replace('5', '')
+                                    .replace('6', '')
+                                    .replace('7', '')
+                                    .replace('8', '')
+                                    .replace('9', '')
+                                )
+                                somewords = someshit.split()
+                                second_word_from_right = somewords[-2]
+                                o.write('import "'+second_word_from_right+'.proto";\n')
+                except IndexError as e:
+                    error_msg = f"索引错误：{e}\n"
+                    error_msg += f"文件路径：{file_to_open}\n"
+                    for i, line in enumerate(lines_set):
+                        if '<code>' in line:
+                            error_msg += f"出错行号：{i+1}\n"
+                            error_msg += line + '\n'
+                    logging.error(error_msg)
+                
             doimport()
             o.write(f'\nmessage {shit} '+"{\n")
             for line in lines_set:
